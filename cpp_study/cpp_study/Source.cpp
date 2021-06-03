73a69ec3888bd40a6453d873450dfffa4a2b6caa
Jeonghyun Jo <jeonghyunjo@berkeley.edu>
2021-06-03 22:38:24 +0900

    windows test

---
+++ cpp_study/cpp_study/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <cmath> //std::pow(base, exponent), M_PI = pi
+#include <cstddef> // to use size_t (i.e. 
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
---
+++ cpp_study/cpp_study/main.h
@@ -0,0 +1,7 @@
+#pragma once
+
+int printout()
+{
+	std::cout << "This is from main.h" << std::endl;
+	return 0;
+}
