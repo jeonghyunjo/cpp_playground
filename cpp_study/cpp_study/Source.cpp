00dfc820bd1fc424c129199ab1613e8c5bb24f67
Jeonghyun Jo <jeonghyunjo@berkeley.edu>
2021-06-22 15:20:59 +0900

    MCL

--- cpp_study/cpp_study/Source.cpp
+++ cpp_study/cpp_study/Source.cpp
@@ -7,19 +7,47 @@
 #include <math.h>
 #include <cstddef> // to use size_t (i.e. 
 #include <string>
-#include "main.h"
+
+#include <tuple>
+
 using namespace std;
 
-int main(int argc, char** argv) {
-    Shape sh1(4, 4);    // Declare shape1
-    Shape sh2(2, 6);    // Declare shape2
-    Shape sh3;          //Declare shape3
+double new_mean, new_var;
 
-    int total = sh1 + sh2;
-    cout << "\nsh1.Area() = " << sh1.Area();
-    cout << "\nsh2.Area() = " << sh2.Area();
-    cout << "\nTotal = " << total;
-    return 0;
-    
+tuple<double, double> measurement_update(double mean1, double var1, double mean2, double var2)
+{
+    new_mean = (var2 * mean1 + var1 * mean2) / (var1 + var2);
+    new_var = 1 / (1 / var1 + 1 / var2);
+    return make_tuple(new_mean, new_var);
+}
+
+tuple<double, double> state_prediction(double mean1, double var1, double mean2, double var2)
+{
+    new_mean = mean1 + mean2;
+    new_var = var1 + var2;
+    return make_tuple(new_mean, new_var);
+}
+
+int main()
+{
+    //Measurements and measurement variance
+    double measurements[5] = { 5, 6, 7, 9, 10 };
+    double measurement_sig = 4;
+
+    //Motions and motion variance
+    double motion[5] = { 1, 1, 2, 1, 1 };
+    double motion_sig = 2;
+
+    //Initial state
+    double mu = 0;
+    double sig = 1000;
+
+    for (int i = 0; i < sizeof(measurements) / sizeof(measurements[0]); i++) {
+        tie(mu, sig) = measurement_update(mu, sig, measurements[i], measurement_sig);
+        printf("update:  [%f, %f]\n", mu, sig);
+        tie(mu, sig) = state_prediction(mu, sig, motion[i], motion_sig);
+        printf("predict: [%f, %f]\n", mu, sig);
+    }
+
     return 0;
 }
---
+++ cpp_study/cpp_study/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello world!" << endl;
+
+	string aaa = "helloworld";
+	char bbb = 'a';
+
+
+	cout << aaa[0] << endl;
+	return 0;
+}
